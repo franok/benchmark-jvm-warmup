18a7e1087d618f7d31647869b8e4ba0775ae1c14
franok <dev@franok.de>
2020-08-18 20:47:36 +0200

    constants can be overwritten by jmh commandline flags

--- src/main/java/de/franok/Constants.java
+++ src/main/java/de/franok/Constants.java
@@ -1,9 +1,9 @@
 package de.franok;
 
 public class Constants {
-    public static final int BENCHMARK_ITERATIONS = 100;//25000;
-    public static final int BENCHMARK_FORKS = 3;
-    public static final int BENCHMARK_WARMUPS = 0;
+    public static final int BENCHMARK_ITERATIONS = 10; // overwritten by jmh command line flag -i <int>
+    public static final int BENCHMARK_FORKS = 3; // overwritten by jmh command line flag -f <int>
+    public static final int BENCHMARK_WARMUPS = 0; // overwritten by jmh command line flag -wi <int>
 
     // SPECjvm2008 constants
     public static final String SPEC_JVM_2008_BASE_PATH = "src/main/java/de/franok/specjvm2008";
