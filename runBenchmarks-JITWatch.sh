32bfb79a867bd92871f161803c9f6e9925280c2f
franok <dev@franok.de>
2020-11-01 22:03:49 +0000

    generate jitwatch logs openj9-aot

--- runBenchmarks-JITWatch.sh
+++ runBenchmarks-JITWatch.sh
@@ -12,6 +12,9 @@ mkdir "$JW$DATE_TIME"
 #~/jvm-evaluation/openj9_jdk-11.0.8+10/bin/java -XX:+HeapDumpOnOutOfMemoryError -jar target/benchmarks.jar Backtracking -jvmArgs "-Xms5g -Xmx5g -XX:+HeapDumpOnOutOfMemoryError -Xgcpolicy:nogc -Xjit:count=0,verbose={compileEnd|inlining|compileTime},vlog=jitwatch-vlog" -rf json -rff jmh-result-openj9-backtracking-sudoku.json -i 21000 -f 1 -to 360m | tee output-openj9-backtracking-sudoku-$(date +'%FT%H-%M').log
 #~/jvm-evaluation/graalvm-ce-java11-20.2.0/bin/java -XX:+HeapDumpOnOutOfMemoryError -jar target/benchmarks.jar Backtracking -jvmArgs "-Xms5g -Xmx5g -XX:+HeapDumpOnOutOfMemoryError -XX:+UnlockExperimentalVMOptions -Dlibgraal.MaxNewSize=6442450944 -Dlibgraal.PrintGC=true -XX:+AlwaysPreTouch -XX:+UnlockDiagnosticVMOptions -XX:+TraceClassLoading -XX:+LogCompilation -XX:LogFile=jitwatch-backtracking-sudoku_graalvm.log" -rf json -rff jmh-result-graalvm-backtracking-sudoku.json -i 21000 -f 1 -to 360m | tee output-graalvm-backtracking-sudoku-$(date +'%FT%H-%M').log
 
+# openj9-aot
+#~/jvm-evaluation/openj9_jdk-11.0.8+10/bin/java -XX:+HeapDumpOnOutOfMemoryError -jar target/benchmarks.jar Backtracking -jvmArgs "-Xms5g -Xmx5g -XX:+HeapDumpOnOutOfMemoryError -Xgcpolicy:nogc -Xshareclasses:nonpersistent,verboseAOT -Xjit:count=0,verbose={compileEnd|inlining|compileTime},vlog=jitwatch-vlog" -rf json -rff jmh-result-openj9-backtracking-sudoku.json -i 21000 -f 1 -to 360m | tee output-openj9-backtracking-sudoku-$(date +'%FT%H-%M').log
+
 mv jmh-result-* "$JW$DATE_TIME"
 mv output-* "$JW$DATE_TIME"
 mv jitwatch-* "$JW$DATE_TIME"
